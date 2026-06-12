77d9aa348496737de74a088c4c17e207de25f907
vrvtvy <53304228+vrvtvy@users.noreply.github.com>
2026-06-12 23:45:48 +0800

    修复：亮色主题不生效（未导入light.css）

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/variables.css'
 import './styles/dark.css'
+import './styles/light.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
